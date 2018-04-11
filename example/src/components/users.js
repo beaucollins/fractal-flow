1dda2e177a31ba7216a9c0098747232e17700e98
Robert Collins <beau@collins.pub>
2018-04-11 11:08:00 -0700

    single socketio component

--- example/src/components/users.js
+++ example/src/components/users.js
@@ -1,6 +1,8 @@
 // @flow
 import type { Component } from 'fractal';
-import socketIOComponent from './socket-io-dep';
+import { createBufferedDispatcher } from 'fractal';
+import type { SocketIOSocket, SocketIONamespace } from './socket-io';
+import createSocketComponent, { createSocketActionListener } from './socket-io';
 
 type UserID = string;
 export type User = { id: UserID };
@@ -21,40 +23,54 @@ type SocketActionType
 
 export type UserComponent = Component<Action, Signal>;
 
-export opaque type SocketIONamespace = any;
-export opaque type SocketIOSocket = any;
+type Authenticator = (SocketIOSocket) => Promise<User>;
 
-export default (namespace: SocketIONamespace, onConnection: (SocketIOSocket) => Promise<void>): UserComponent => socketIOComponent( {
-	namespace,
-	onConnection,
-	effectHandler: ( effect ) => {
-		if ( effect.type ) {
-			switch( effect.type ) {
-			case 'sync':
-				break;
-			case 'emit':
-				( effect.to
-					? namespace.to( effect.to )
-					: namespace )
-					.emit( effect.name, ... effect.args );
-				break;
-			case 'socket':
-				switch ( effect.action.type ) {
+export default function(namespace: SocketIONamespace, authenticator: Authenticator): UserComponent {
+	const component = createSocketComponent( namespace );
+	// configure the component to subscribe to a socket and emit specific actions
+	return ( dispatch ) => {
+		component( activity => {
+			// set up some emit action dispatchers?
+			const auth = authenticator( activity.socket );
+			const bufferedDispatch = createBufferedDispatcher( auth, dispatch );
+
+			const listenFor = ( eventName: string, action: ( ... mixed[] ) => (User => Action) ) =>
+				createSocketActionListener( activity.socket, eventName, bufferedDispatch, action );
+
+			listenFor( 'grow', () => {
+				return user => ( { user, action: 'grow' } );
+			} );
+		} );
+
+		// currently we don't know what to do with ourselves
+		return effect => {
+			if ( effect.type ) {
+				switch( effect.type ) {
+				case 'sync':
+					break;
 				case 'emit':
-					effect.socket.emit( effect.action.name, ... effect.action.args );
+					( effect.to
+						? namespace.to( effect.to )
+						: namespace )
+						.emit( effect.name, ... effect.args );
 					break;
-				case 'leave':
-					effect.socket.leave( effect.action.room );
-					break;
-				case 'join':
-					effect.socket.join( effect.action.room );
-					break;
-				case 'disconnect':
-					effect.socket.disconnect();
-					break;
+				case 'socket':
+					switch ( effect.action.type ) {
+					case 'emit':
+						effect.socket.emit( effect.action.name, ... effect.action.args );
+						break;
+					case 'leave':
+						effect.socket.leave( effect.action.room );
+						break;
+					case 'join':
+						effect.socket.join( effect.action.room );
+						break;
+					case 'disconnect':
+						effect.socket.disconnect();
+						break;
+					}
 				}
 			}
-			return;
-		}
-	},
-} );
+		};
+	};
+}
